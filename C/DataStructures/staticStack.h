4eca1fa419cac903082e87f7e4d4bdf2ba403c62
Pablo <pablomonardo@gmail.com>
2023-01-17 11:38:12 -0300

    Update staticStack.h

--- C/DataStructures/staticStack.h
+++ C/DataStructures/staticStack.h
@@ -1,7 +1,7 @@
 #ifndef STATICSTACK_H_INCLUDED
 #define STATICSTACK_H_INCLUDED
 
-typedef struct 
+typedef struct
 {
     int vector[10];
     int top;
@@ -9,10 +9,10 @@ typedef struct
 
 
 STACK initialize();
-int is_full(STACK stack);
-void push(STACK stack, int number);
-void peack(STACK stack);
-void pop(STACK stack);
+int is_full(STACK *stack);
+void push(STACK *stack, int number);
+void peack(STACK *stack);
+void pop(STACK *stack);
 
 
 #endif // STATICSTACK_H_INCLUDED
