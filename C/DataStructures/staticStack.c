8a1777d6a9ccab20ad88be84ec7d5b41eadc0c68
Pablo <pablomonardo@gmail.com>
2023-01-17 10:13:47 -0300

    refactor the name

---
+++ C/DataStructures/staticStack.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct stack
+{
+    int vector[10];
+    void *top;
+};
+
+
+//Callbacks
+void initialize();
+
+int push();
+void peack();
+int pop();
+
+
+int main()
+{
+
+}
+
+
+void initialize()
+{
+
+}
+
