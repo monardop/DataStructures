2211d9d1761f7da62da71a44a2fdd4dc47b6bba3
Pablo <pablomonardo@gmail.com>
2023-01-17 11:07:50 -0300

    Finished the functions

--- C/DataStructures/staticStack.c
+++ C/DataStructures/staticStack.c
@@ -5,10 +5,40 @@
 STACK initialize()
 {
     STACK new;
-    new.top = &new.vector[0];
+    new.top = 0;
 
     return new;
 }
 
 
+int is_full(STACK stack)
+{
+    return stack.top + 1 == 10; 
+}
 
+
+void push(STACK stack, int number)
+{
+    if(!is_full(stack))
+    {
+        stack.top++;
+        stack.vector[stack.top] = number;
+    }
+    else
+    {
+        printf("You can't push more elements, stack full.\n");
+    }
+}
+
+
+void peack(STACK stack)
+{
+    printf("%d", stack.vector[stack.top]);
+}
+
+
+void pop(STACK stack)
+{
+    peack(stack);
+    stack.top--;
+}
