ff097ff8e1b7df724e281a942aca4be2af487700
Pablo <pablomonardo@gmail.com>
2024-04-20 19:24:09 -0300

    Add some testing

--- Exercises/StackFileSort/main.c
+++ Exercises/StackFileSort/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../../Stack/Dynamic/DynamicStack.h"
+
+
 
 int main()
 {
     printf("Hello world!\n");
     return 0;
+}
+
+int case1(void)
+{
+    int vec[] = {4, 10, 16, 17, 18, 19, 29, 31, 45, 51, 52, 
+                        53, 55, 59, 70, 85, 86, 89, 90, 93};
+    
+}
+
+int case2(void)
+{
+    int vec[] = {0, 5, 6, 8, 12, 29, 31, 32, 36, 41, 42, 50, 
+                             57, 63, 68, 75, 87, 90, 93, 99};
 }
