c0864ae85a0c1393c8e30e9c0932a97998a0c4fe
Pablo <pablomonardo@gmail.com>
2024-04-27 22:43:34 -0300

    Added some functions
    It just works

--- Utilities/utilities.c
+++ Utilities/utilities.c
@@ -1,4 +1,6 @@
-void *memoryCopy(void *dest, const void *src,  unsigned size) 
+#include <stdio.h>
+
+void *memoryCopy(void *dest, const void *src,  unsigned size)
 {
     const unsigned char *source = (const unsigned char*)src;
     char unsigned *destination = (unsigned char*)dest;
@@ -24,9 +26,63 @@ int memoryCompare(const char *elementA, const char *elementB, unsigned bytes)
     return 1;
 }
 
-int strLen(const char *str) 
+int strLen(const char *str)
 {
     const char *pStr = str;
     while (*pStr++);
     return pStr - str - 1;
+}
+
+int intCompare(void *elemenA, void *elementB)
+{
+    int *a = (int *)elemenA;
+    int *b = (int *)elementB;
+
+    return *a - *b;
+}
+
+int strCompare(void *elementA, void *elementB)
+{
+    char *a = (char *)elementA;
+    char *b = (char *)elementB;
+
+    int comparison = 0;
+
+    while (*a)
+    {
+        comparison += *a;
+        a++;
+    }
+    while (*b)
+    {
+        comparison -= *b;
+        b++;
+    }
+
+    return comparison;
+}
+
+int floatCompare(void *elementA, void *elementB)
+{
+    float *a = (float *)elementA;
+    float *b = (float *)elementB;
+    return (int)(*a - *b);
+}
+
+void intPrint(void* number)
+{
+    int *x = (int *)number;
+    printf("\t%d", *x);
+}
+
+void floatPrint(void* number)
+{
+    float *x = (float *)number;
+    printf("\t%f", *x);
+}
+
+void strPrint(void* string)
+{
+    char *x = (char *)string;
+    printf("\t%s", x);
 }
--- Utilities/utilities.h
+++ Utilities/utilities.h
@@ -14,8 +14,21 @@
         struct sNodo *next;
    }tNode;
 
+   typedef int (*cmp)(void *, void *);
+   typedef void (*print)(void *);
+
    void *memoryCopy(void *dst, const void *src, unsigned dataSize);
    int memoryCompare(const char *a, const char *b, unsigned dataSize);
    int strLen(const char *str);
+
+   //Comparing tools
+   int intCompare(void *, void *);
+   int strCompare(void *, void *);
+   int floatCompare(void *, void *);
+
+   //print tools
+   void intPrint(void*);
+   void floatPrint(void*);
+   void strPrint(void*);
 
 #endif // UTILITIES_H_INCLUDED
