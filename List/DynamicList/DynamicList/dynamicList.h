992c67fefd43026f5e6ca5fd2070a4c427e5eef2
Pablo <pablomonardo@gmail.com>
2024-04-30 17:37:19 -0300

    Update dynamicList.h
    Added some new callouts

--- List/DynamicList/DynamicList/dynamicList.h
+++ List/DynamicList/DynamicList/dynamicList.h
@@ -7,12 +7,18 @@
     typedef tNode *dsList;
 
     void newList(dsList *);
-    int isEmpty(dsList *);
+    bool isEmpty(dsList *);
     void clearList(dsList *);
     int append(dsList *, void *, unsigned);
-    int sortedInsert(dsList *, void *, unsigned, cmp);
     int listLen(dsList *);
+    int sortedInsert(dsList *, void *, unsigned, cmp);
     void sortList(dsList *, cmp);
     void printList(dsList *, print);
+    int removeFirst(dsList *, void *, unsigned );
+    int removeAllOccurrences(dsList *, void *, unsigned);
+    int removeLastOccurrence(dsList *plist, void *data, unsigned dataSize);
+    int removeIndex(dsList *pList, int index);
 
+
+    
 #endif // DYNAMICLIST_H
