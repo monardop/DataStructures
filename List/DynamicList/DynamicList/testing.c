3ace8dd3cc94ca3f63cd613701464fd2155572c8
Pablo <pablomonardo@gmail.com>
2024-04-30 17:36:57 -0300

    Update testing.c
    Added a new test

--- List/DynamicList/DynamicList/testing.c
+++ List/DynamicList/DynamicList/testing.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "testing.h"
 
-int test_append()
+bool test_append()
 {
     int num[] = {0,1,2,3,4,5,6,7,8,9};
     dsList list;
@@ -14,10 +14,10 @@ int test_append()
     printList(&list, intPrint);
     printf("\nList len: %d", listLen(&list));
     clearList(&list);
-    return 1;
+    return True;
 }
 
-int test_sortedAppend()
+bool test_sortedAppend()
 {
     int numSorted[] = {0,4,8,12,16,20,24};
     int num[] = {6,3,2,5,7,9,1,8,10,23};
@@ -42,7 +42,7 @@ int test_sortedAppend()
     return 1;
 }
 
-int test_sort()
+bool test_sort()
 {
     int num[] = {6,3,2,5,7,9,1,8,10,23};
     dsList list;
@@ -60,15 +60,61 @@ int test_sort()
     printList(&list, intPrint);
 
     clearList(&list);
-    return 1;
+    return True;
 }
+
+bool test_remove()
+{
+    int test[] = {4,5,3,1,9,1,2,1,3,7,2,9,4,0,4};
+    dsList testList;
+    int delNumber;
+
+    newList(&testList);
+
+    for (int i = 0; i < 15; i++)
+    {
+        append(&testList, test+i, sizeof(int));
+    }
+    
+    printf("Elements: %d\n", listLen(&testList));
+    printList(&testList, intPrint);
+
+    printf("\nRemoving all 1's\n");
+    delNumber = 1;
+    removeAllOccurrences(&testList, &delNumber, sizeof(int));
+    printList(&testList, intPrint);
+
+    printf("\nRemoving the last 4\n");
+    delNumber = 4;
+    removeLastOccurrence(&testList, &delNumber, sizeof(int));
+    printList(&testList, intPrint);
+
+    printf("\nRemoving the first 9\n");
+    delNumber = 9;
+    removeFirst(&testList, &delNumber, sizeof(int));
+    printList(&testList, intPrint);
+    printf("\nRemoving the 3rd element\n");
+ 
+    removeIndex(&testList, 3);
+    printList(&testList, intPrint);
+
+    clearList(&testList);
+    
+    return True;
+}
+
 
 void mainTesting(void)
 {
     printf("Testing Append:\n");
     test_append();
+    printf(SEPARATOR);
     printf("\nTesting sortedInsert:\n");
     test_sortedAppend();
+    printf(SEPARATOR);
     printf("\nTesting sort:\n");
     test_sort();
+    printf(SEPARATOR);
+    printf("\n\nTesting remove:\n");
+    test_remove();
 }
