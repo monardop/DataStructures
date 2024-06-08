1b818998b57e2acc0c45b37002ff7e84fa6a1d95
Pablo <pablomonardo@gmail.com>
2024-06-08 18:42:10 -0300

    refactor: create a new func that prints the test results

--- Tree/BinaryTree/test_tree.c
+++ Tree/BinaryTree/test_tree.c
@@ -1,13 +1,25 @@
 #include "testing.h"
 #include <stdio.h>
 
+void testMessage(char *msg, int result, int expected)
+{   
+    printf("\n%s\n", msg);
+    if (result == expected)
+    {
+        printf("\t\t OK\n");
+    }else
+    {
+        printf("\t\t ERROR\n");
+    }
+}
+
 void intSquare(void *element)
 {
     int *newElement = (int *)element;
     *newElement *= *newElement;
 }
 
-int test_recursiveInsert()
+void test_recursiveInsert()
 {
     tree test;
     int num[] = {100, 50, 200, 25, 75, 80, 50};
@@ -34,11 +46,9 @@ int test_recursiveInsert()
     printInOrder(&test, intPrint);
 
     clearTree(&test);
-    
-    return OK;
 }
 
-int new_test()
+void new_test()
 {
     tree testTree;
     int testing[] = {67, 52, 41,4, 13, 31, 38, 43,65,60,66,94,77,95,96};
@@ -53,46 +63,29 @@ int new_test()
     
     clearTree(&testTree);
 
-    return OK;
 }
 
 void test_avl()
 {
-    tree testTree1, testTree2;
+    tree testTree1;
     int testNum1[] = {50, 17, 76, 9, 23, 54, 72, 67, 14, 19, 12};
     int testNum2[] = {50, 17, 72, 12, 23, 54, 76, 9, 14, 19, 67};
 
     // test False
     newTree(&testTree1);
-    printf("\nTesting AVL false:\n");
     for(int i = 0; i < 11; i++)
     {
         recursiveInsert(&testTree1, testNum1 + i, sizeof(int), intCompare);
     }
-    if (avlTree(&testTree1) == False)
-    {
-        printf("\t\t OK\n");
-    }else
-    {
-        printf("\t\t ERROR\n");
-    }
+    testMessage("Testing AVL -> False",avlTree(&testTree1),False);
     clearTree(&testTree1);
     
-    newTree(&testTree2);
-    printf("\nTesting AVL true:\n");
     for(int i = 0; i < 11; i++)
     {
-        recursiveInsert(&testTree2, testNum2 + i, sizeof(int), intCompare);
+        recursiveInsert(&testTree1, testNum2 + i, sizeof(int), intCompare);
     }
-    if (avlTree(&testTree2) == True)
-    {
-        printf("\t\t OK\n");
-    }else
-    {
-        printf("\t\t ERROR\n");
-    }
-    clearTree(&testTree2);
-    
+    testMessage("Testing AVL -> True", avlTree(&testTree1), True);
+    clearTree(&testTree1);
 }
 
 void test_complete()
@@ -131,6 +124,38 @@ void test_complete()
         printf("\t\t ERROR\n");
     }
     clearTree(&testTree2);
+}
+
+void test_balanced()
+{
+    tree testTree;
+    int testNum1[] = {50, 17, 72, 12, 23, 54, 76, 9, 14, 19, 67}, // true
+        testNum2[] = {100, 50, 200, 30, 60, 150, 280}, // true
+        testNum3[] = {50, 17, 72, 12, 23, 54, 76, 9, 14, 19, 67, 3}; // false
+
+    newTree(&testTree);
+
+
+    for (int i = 0; i < 11; i++)
+    {
+        recursiveInsert(&testTree, testNum1 + i, sizeof(int), intCompare);
+    }
+    testMessage("Testing balanced -> True", avlTree(&testTree), True);
+    clearTree(&testTree);
+
+    for (int i = 0; i < 11; i++)
+    {
+        recursiveInsert(&testTree, testNum2 + i, sizeof(int), intCompare);
+    }
+    testMessage("Testing balanced -> True", avlTree(&testTree), True);
+    clearTree(&testTree);
+
+    for (int i = 0; i < 11; i++)
+    {
+        recursiveInsert(&testTree, testNum3 + i, sizeof(int), intCompare);
+    }
+    testMessage("Testing balanced -> False", avlTree(&testTree), False);
+    clearTree(&testTree);
 }
 
 void mainTest(void)
