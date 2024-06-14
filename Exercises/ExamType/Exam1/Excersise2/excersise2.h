a45d32c4830dc563ff28feaf0e43a3cff4ef05c3
Pablo <pablomonardo@gmail.com>
2024-06-14 12:33:43 -0300

    feat: added tree func

--- Exercises/ExamType/Exam1/Excersise2/excersise2.h
+++ Exercises/ExamType/Exam1/Excersise2/excersise2.h
@@ -1,6 +1,32 @@
 #ifndef EXCERSISE2_H
 #define EXCERSISE2_H
 
+    #define NOT_FOUND -1
+    #define DUPLICATE -1
+    #define OK 1
+    #define ERROR 0
+
+    #include <stdlib.h>
+    #include <string.h>
+
+    typedef struct Node
+    {
+        void *info;
+        unsigned dataSize;
+        struct Node *left, *right;
+    }tNode;
+
+    typedef tNode *tree;
+    typedef int (*cmp)(void *, void *);
+    typedef void (*action)(void *);
+
+
+    void newTree(tree *tp);
+    int isEmpty(tree *tp);
+    int addNode(tree *tp, void *info, unsigned dataSize, cmp cmp);
+    void clearTree(tree *tp);
+    void inorder(tree *tp, action func);
+    tNode *searchNotKey(tree *tp, void *searchedValue,cmp cmp);
 
 
 #endif // EXCERSISE2_H
