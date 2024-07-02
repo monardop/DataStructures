6facf1995d996e799cb8237c800869ccb9fbfc45
Pablo <pablomonardo@gmail.com>
2024-07-02 10:53:07 -0300

    Al functions declared

--- Exercises/ExamType/Exam2/listInterface.h
+++ Exercises/ExamType/Exam2/listInterface.h
@@ -1,7 +1,27 @@
 #ifndef CIRCULARLIST_H
 #define CIRCULARLIST_H
 
-    
+    #include <stdlib.h>
+    #include <string.h>
+
+    #include "utilities.h"
+
+    typedef struct sNode
+    {
+        void         *data;
+        int           dataSize;
+        struct sNode *next;
+    }tNode;
+
+    typedef tNode* dsList;
+
+    void newList(dsList *list);
+    int insertElement(dsList *list, const void *data, const int dataSize);
+    void clearList(dsList *list);
+    dsList *findElem(dsList *list, const void *data, cmp cmp);
+    void mapList(dsList *list, action function);
+
+
 
 
 #endif
