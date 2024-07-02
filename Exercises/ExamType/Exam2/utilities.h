cc291cd8edc1c6d73a99f6f9c3b90c36e928c013
Pablo <pablomonardo@gmail.com>
2024-07-02 10:51:40 -0300

    feat: added some error handling

--- Exercises/ExamType/Exam2/utilities.h
+++ Exercises/ExamType/Exam2/utilities.h
@@ -1,6 +1,12 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+    #define OK         1
+    #define MEM_FAIL   0
+    #define FILE_FAIL -1
+
+    typedef int  (*cmp)(const void *, const void *);
+    typedef void (*action)(void *);
 
 
 #endif // UTILITIES_H
