106784b017a9e985fdfd71e9cb7848f708aa3ab3
Pablo <pablomonardo@gmail.com>
2024-07-03 10:21:20 -0300

    Created an utilities module

---
+++ Tree/IndexTree/utilities.h
@@ -0,0 +1,12 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+
+    #define OK          1
+    #define DUPLICATED -1
+    #define MEM_FAIL    0
+
+    typedef enum{False, True}boolType;
+
+
+#endif // UTILITIES_H
