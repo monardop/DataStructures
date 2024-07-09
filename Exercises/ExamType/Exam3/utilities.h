c0e0c9a9384c640c3625a0bfb5bfbfbfc99f9733
Pablo <pablomonardo@gmail.com>
2024-07-09 18:25:27 -0300

    feat: update utilities

--- Exercises/ExamType/Exam3/utilities.h
+++ Exercises/ExamType/Exam3/utilities.h
@@ -1,6 +1,17 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+    typedef struct
+    {
+        int day, month, year;
+    } tDate;
 
+    #define OK 1
+    #define MEM_ERROR 0
+    #define FILE_ERROR -1
+
+    int  isValidDate(tDate date);
+    void getExpireDate(tDate *date, int n);
+    void errorHandling(int error);
 
 #endif // UTILITIES_H
