690b9614521eff6be10633b05b39ce6ab7a76c58
Pablo <pablomonardo@gmail.com>
2024-07-09 18:26:04 -0300

    feat: update header

--- Exercises/ExamType/Exam3/list_interface.h
+++ Exercises/ExamType/Exam3/list_interface.h
@@ -1,6 +1,20 @@
 #ifndef LIST_INTERFACE_H
 #define LIST_INTERFACE_H
 
+    typedef struct sNode {
+        void         *data;
+        unsigned      dataSize;
+        struct sNode *next;
+    }tNode;
 
+    typedef tNode *dsList;
+    typedef int (*cmp)(const void *, const void*);
+
+    void newList(dsList *);
+    void delList(dsList *pl);
+    int  emptyList(dsList *);
+    int  find(dsList *pl, void *key, cmp cmp);
+    int  sortedInsertion(dsList *pl, void *data, unsigned dataSize, cmp cmp);
+    
 
 #endif // LIST_INTERFACE_H_INCLUDED
