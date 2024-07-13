45d3bce252d1473b21e20c0f47837a43ae2868ff
Pablo <pablomonardo@gmail.com>
2024-07-13 17:12:31 -0300

    feat: logic finished

--- Exercises/ProductStatus/check_file.c
+++ Exercises/ProductStatus/check_file.c
@@ -1,1 +1,102 @@
+#include <stdio.h>
+#include <string.h>
 
+#include "check_file.h"
+
+FILE* checkIntegrity(char *fileName, char *mode) {
+    FILE *fp;
+
+    if((fp = fopen(fileName, mode)) == NULL) {
+        printf("There was an error with '%s'", fileName);
+        return NULL;
+    }
+
+    return fp;
+}
+
+void updateProblemFile(FILE *pf, dsStack *sp) {
+    Product aux;
+
+    while(pull(sp, &aux)) {
+        fprintf(pf, "%s\t%s\t%d\t%c", aux.cod, aux.description, aux.quantity, aux.operation);
+        fprintf(pf,"\n");
+    }
+}
+
+void updateOkFile(FILE *pf, dsStack *sp, int final) {
+    Product aux;
+    pull(sp, &aux);
+
+    aux.quantity = final;
+    aux.operation = '-';
+    fwrite(&aux, sizeof(Product), 1, pf);
+    cleanStack(sp);
+
+}
+
+int analizeFile(void) {
+    FILE *baseFile, *withProblem, *ok;
+    dsStack stack;
+    Product aux;
+    char auxCod[6];
+    int quantityOfProducts = 0;
+
+    baseFile = checkIntegrity(MOVEMENTS_FILE, "rb");
+    if(!baseFile) return FAIL;
+    ok = checkIntegrity(OK_FILE, "wb");
+    if(!ok) {
+        fclose(baseFile);
+        return FAIL;
+    }
+    withProblem = checkIntegrity(ERROR_FILE, "wt");
+    if(!withProblem) {
+        fclose(baseFile);
+        fclose(ok);
+        return FAIL;
+    }
+
+    initialize(&stack);
+    fread(&aux, sizeof(Product), 1, baseFile);
+    while (!feof(baseFile)) {
+        strcpy(auxCod, aux.cod);
+        while (strcmp(auxCod, aux.cod) == 0) {
+            if(aux.operation == 'I') quantityOfProducts += aux.quantity;
+            else quantityOfProducts -= aux.quantity;
+
+            push(&stack, &aux, sizeof(Product));
+            fread(&aux, sizeof(Product), 1, baseFile);
+        }
+        if(quantityOfProducts >= 0) updateOkFile(ok, &stack, quantityOfProducts);
+        else updateProblemFile(withProblem,&stack);
+    }
+    
+    return OK;
+}
+
+int printBinaryFile(char *fileName) {
+    Product aux;
+    FILE *pf;
+
+    pf = checkIntegrity(fileName, "rb");
+    if(!pf) return FAIL;
+
+    while(fread(&aux, sizeof(Product), 1, pf)) {
+        fprintf(stdout, "%s\t%s\t%d\t%c", aux.cod, aux.description, aux.quantity, aux.operation);
+        fprintf(stdout,"\n");
+    }
+
+    return OK;
+}
+
+int printTextFile(char *fileName) {
+    char buffer[100];
+    FILE *pf;
+
+    pf = checkIntegrity(fileName, "rt");
+    if(!pf) return FAIL;
+
+    while(fgets(buffer,100, pf)) {
+        printf("%s", buffer);
+    }
+    return OK;
+}
--- Exercises/ProductStatus/check_file.h
+++ Exercises/ProductStatus/check_file.h
@@ -1,6 +1,20 @@
 #ifndef CHECK_FILE_H
 #define CHECK_FILE_H
+    #include "stack_structure.h"
 
+    #define MOVEMENTS_FILE "movements.dat"
+    #define ERROR_FILE     "wrong_input.txt"
+    #define OK_FILE        "products.dat"
 
+    typedef struct {
+        char cod[6];
+        char description[25];
+        int  quantity;
+        char operation; // I/O
+    }Product;
+
+    int analizeFile(void);
+    int printBinaryFile(char *fileName);
+    int printTextFile(char *fileName);
 
 #endif // CHECK_FILE_H
