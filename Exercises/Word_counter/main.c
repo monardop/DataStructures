851d2192e3c2ce635235ee9829b55a33b567f748
Pablo <pablomonardo@gmail.com>
2024-07-16 19:47:15 -0300

    created a menu

--- Exercises/Word_counter/main.c
+++ Exercises/Word_counter/main.c
@@ -1,8 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+
+#include "system_interface.h"
+#include "binary_tree.h"
+
 int main()
 {
+    int selection;
+    Tree wordTree;
+    initializeTree(&wordTree);
+
     setlocale(LC_ALL,"spanish");
+
+    while (1){
+        selection = menu();
+        switch (selection)
+        {
+        case 1:
+            readFile(&wordTree);
+            break;
+        case 2:
+            showWords(&wordTree);
+            break;
+        case 3:
+            mostOccurrences(&wordTree);
+            break;
+        case 4:
+            fewerOccurrences(&wordTree);
+            break;
+        case 5: 
+            exitProgram(&wordTree);
+            return 0;
+        default:
+            printf("Por favor ingrese un número del 1 al 5\n");
+            break;
+        }
+        system("pause");
+        system("cls");
+    }
+    cleanTree(&wordTree);
     return 0;
 }
