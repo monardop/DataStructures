d316b20a93f33a64bbd197299b209939f6baa7b8
Pablo <pablomonardo@gmail.com>
2024-07-15 23:26:55 -0300

    feat: added new functions

--- Exercises/Word_counter/word.c
+++ Exercises/Word_counter/word.c
@@ -1,9 +1,7 @@
-#include "word.h"
-#include "binary_tree.h"
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include "word.h"
 
 
 void duplicatedWord(void *data) {
@@ -15,7 +13,7 @@ int compareWords(const void *word1, const void *word2) {
     Word *elem1 = (Word *)word1;
     Word *elem2 = (Word *)word2;
 
-    return strcmp(elem1->word, elem2->word);
+    return strcmpi(elem1->word, elem2->word);
 }
 
 int compareOccurrences(const void *word1, const void *word2) {
@@ -30,3 +28,44 @@ void printWord(void *data) {
     printf("Word: %s. Occurrences: %u ", element->word, element->occurrences);
 }
 
+int createWord(char *word, unsigned len, Tree *tp) {
+    Word newWord;
+
+    newWord.occurrences = 1;
+    memcpy(newWord.word, word, len);
+    return newElement(tp, (void *)&newWord, sizeof(Word), compareWords, duplicatedWord);
+}
+
+int parceWords(char *sentence, Tree *tp) {
+    char *endp, *startp;
+
+    startp = endp = strchr(sentence, '\n');
+    *endp = '\0'; 
+
+    while (startp != sentence){
+        startp--;
+        if(*startp == ' '){
+            if (createWord(startp + 1, endp - startp, tp) != OK) {
+                return MEM_ERROR;
+            }
+            endp = startp;
+            *endp = '\0'; 
+        }
+    }
+    if (createWord(startp + 1, endp - startp, tp) != OK) {
+        return MEM_ERROR;
+    }
+    return OK;
+}
+
+int getWords(FILE *fp, Tree *tp) {
+    char buffer[BUFFER_LEN];
+
+    while(fgets(buffer, BUFFER_LEN,fp)) {
+        if(parceWords(buffer, tp) != OK) {
+            return MEM_ERROR;
+        }
+    }
+
+    return OK;
+}
--- Exercises/Word_counter/word.h
+++ Exercises/Word_counter/word.h
@@ -1,12 +1,16 @@
 #ifndef WORDS_H
 #define WORDS_H
 
+    #include <stdio.h>
+
+    #include "binary_tree.h"
+
     typedef struct {
         char word[50];
         unsigned occurrences;
     }Word;
 
-    #define BUFFER_LEN  100;
+    #define BUFFER_LEN  100
     
 
 
