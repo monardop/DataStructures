8cc8d41e25db2b2ec575217e9a6e19fda3aba42d
Pablo <pablomonardo@gmail.com>
2024-10-14 22:23:29 -0300

    Modifico la clase SLL y la testeo

--- Python/List.py
+++ Python/List.py
@@ -1,4 +1,6 @@
 from Nodes import OnePointerNode
+from random import randint
+
 
 class SimpleLinkedList:
     def __init__(self) -> None:
@@ -6,20 +8,38 @@ class SimpleLinkedList:
 
     def push_node(self, data):
         new_node = OnePointerNode(data)
-
-        if self.header is None:
+        if self.header == None:
             self.header = new_node
             return
-        else:
-            new_node.next = self.header
-            self.header = new_node
-            return
+        new_node.next = self.header
+        self.header = new_node
 
-    def print_list(self, header: OnePointerNode):
-        while header is not None:
-            if header.next is None:
-                print(header.data)
+    def is_in_list(self, data) -> bool:
+        pointer = self.header
+
+        while pointer != None:
+            if data == pointer.data:
+                return True
+            pointer = pointer.next
+
+        return False
+
+    def print_list(self):
+        pointer = self.header
+        while pointer is not None:
+            if pointer.next is None:
+                print(pointer.data)
                 return
-            print(header.data, end=" ")
-            header = header.next
+            print(pointer.data, end=" ")
+            pointer = pointer.next
 
+
+lista = SimpleLinkedList()
+
+for _ in range(10):
+    numero = randint(0, 100)
+    lista.push_node(numero)
+
+lista.print_list()
+lista.print_list()
+print(lista.is_in_list(numero))
