39622bad14a69a90c62a0d2f315f02060e9361da
Pablo <pablomonardo@gmail.com>
2024-11-28 16:44:11 -0300

    Nuevo ejercicio

---
+++ Exercises/ObtenerFraseMasLarga/VersiónC/examen.h
@@ -0,0 +1,12 @@
+#ifndef EXAMEN_H_INCLUDED
+    #define EXAMEN_H_INCLUDED
+
+    #define MAXLENGHT 1024
+    #define CANTFRASES 5
+
+    #include <string.h>
+
+    char* obtenerFrase(char* frase);
+    void obtener5FrasesMasLargas();
+
+#endif // EXAMEN_H_INCLUDED
